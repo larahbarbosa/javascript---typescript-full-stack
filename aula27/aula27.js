dead91b20a47a901814595ad40897c64c5b29a5d
Larissa Barbosa <larissabarbosa@id.uff.br>
2022-05-23 19:40:20 -0300

    ternary operation

---
+++ aula27/aula27.js
@@ -0,0 +1,15 @@
+//Operador ternário 
+// ? :
+// '?' para condição verdadeira, ':' para condição falsa
+
+//O item nivelUsuário é igual ao if else comentado abaixo
+
+const pontuacaoUsuario = 999; 
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario);
+
+/* if (pontuacaoUsuario >= 1000) {
+    console.log('Usuário VIP');
+} else {
+    console.log('Usuário normal');
+} */
