d108916e011a43ef2b44182ca073be5631766aa9
Larissa Barbosa <larissabarbosa@id.uff.br>
2022-06-21 22:19:49 -0300

    For in

---
+++ aula36/aula36.js
@@ -0,0 +1,29 @@
+//For in lê os índices ou chaves do objeto
+
+/* const fruits = ['Pera', 'Maça', 'Uva'];
+
+for (let i = 0; i < fruits.length; i++) { //acessando todos os itens do array
+    console.log(fruits[i]);
+} */
+
+
+/* for (let i in fruits) { //lendo o índice 
+    console.log(i);
+} */
+
+
+/* for (let indice in fruits) { //lendo as chaves objetos
+    console.log(fruits[indice]);
+} */
+
+const pessoa = {
+    nome: 'Lara',
+    idade: 25
+};
+
+/* console.log(pessoa.nome); //para encontrar alguma coisa
+console.log(pessoa['nome']); */
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
+}
