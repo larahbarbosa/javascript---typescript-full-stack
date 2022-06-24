add841318f2fd3919e25b6734bf8e8fc41581f43
Larissa Barbosa <larissabarbosa@id.uff.br>
2022-06-24 18:51:53 -0300

    continue & break

---
+++ aula40/aula40.js
@@ -0,0 +1,15 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let numero of numeros) {
+    
+    if (numero === 2) { //pulando para próxima iteração do laço
+        continue;
+    }
+    
+    if (numero === 8) {
+        break;
+    }
+
+    console.log(numero);
+
+}
